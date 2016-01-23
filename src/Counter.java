c35ffd6f4224474ee2b35b1634acd79e707561f9
Damian Spinek <damian00737@gmail.com>
2016-01-23 12:37:32 +0100

    Exam result

---
+++ src/Counter.java
@@ -0,0 +1,8 @@
+public class Counter{
+
+    public static int plus(int a, int b){return a+b;}
+public static int minus(int a, int b){return a-b;}
+public static int divide(int a, int b){return a/b;}
+public static int multiple(int a, int b){return a*b;}
+
+}
